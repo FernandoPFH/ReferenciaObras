68fd62ad8e77e6d2b5ed87257eb09bab54e28f70
Fernando Padilha Farah <pensecomigocanal@gmail.com>
2020-05-09 01:49:59 -0300

    Corrigindo

--- Server/rocheckuser/main.py
+++ Server/rocheckuser/main.py
@@ -14,6 +14,7 @@ channel.queue_declare(queue="trylogin")
 def on_request(ch, method, properties, body):
 	while True:
 		try:
+			print("Tentando")
 			mydb = mysql.connector.connect(host="mysql-server", user="fernando", passwd="fernando", database="data.db")
 			break
 		except:
