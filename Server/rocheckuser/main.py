628f0ff6aaeefbb8f900023261ebe534edc400a9
Fernando Padilha Farah <pensecomigocanal@gmail.com>
2020-05-09 01:16:40 -0300

    Corrigindo

--- Server/rocheckuser/main.py
+++ Server/rocheckuser/main.py
@@ -14,7 +14,7 @@ channel.queue_declare(queue="trylogin")
 def on_request(ch, method, properties, body):
 	while True:
 		try:
-			mydb = mysql.connector.connect(host="rabbitmq-server", user="main", passwd="main", database="data.db")
+			mydb = mysql.connector.connect(host="rabbitmq-server", user="fernando", passwd="fernando", database="data.db")
 			break
 		except:
 			time.sleep(5)
