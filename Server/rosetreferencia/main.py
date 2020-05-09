56504c827333b87c3fedc55f805e9bbe2eee4eda
Fernando Padilha Farah <pensecomigocanal@gmail.com>
2020-05-09 17:48:52 -0300

    Corrigindo

--- Server/rosetreferencia/main.py
+++ Server/rosetreferencia/main.py
@@ -31,6 +31,9 @@ def on_request(ch, method, properties, body):
 			comand = "UPDATE Referencia SET Preco = %s WHERE Nome = %s"
 			val = (float(ref[3*i+1]),ref[3*i])
 			mycursor.execute(comand,val)
+			comand = "UPDATE Referencia SET Tempo = %s WHERE Nome = %s"
+			val = (int(ref[3*i+2]),ref[3*i])
+			mycursor.execute(comand,val)
 			mydb.commit()
 
 		resposta = "True"
