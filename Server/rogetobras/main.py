219602145ad158414473737193466cd61dfd5e22
Fernando Padilha Farah <pensecomigocanal@gmail.com>
2020-05-09 11:59:35 -0300

    Debug

--- Server/rogetobras/main.py
+++ Server/rogetobras/main.py
@@ -24,7 +24,8 @@ def on_request(ch, method, properties, body):
 
 	comand = "SELECT * FROM Obras"
 
-	try:
+	#try:
+	if True:
 		mycursor.execute(comand)
 		myresult = mycursor.fetchall()
 
@@ -34,8 +35,8 @@ def on_request(ch, method, properties, body):
 			answer += "!!@!!".join(linha) + "!!!@!!!"
 
 		reposta = "True" + "!@!" + myresult
-	except:
-		resposta = "False"
+	#except:
+	#	resposta = "False"
 
 	ch.basic_publish(exchange='',routing_key=properties.reply_to,properties=pika.BasicProperties(correlation_id = \
 														properties.correlation_id),body=resposta)
