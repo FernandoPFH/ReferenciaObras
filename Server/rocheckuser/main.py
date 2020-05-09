e98dce2c0c0c371f2c57ec76aebb7cc744078aa9
Fernando Padilha Farah <pensecomigocanal@gmail.com>
2020-05-09 02:43:33 -0300

    Corrigindo

--- Server/rocheckuser/main.py
+++ Server/rocheckuser/main.py
@@ -25,6 +25,15 @@ def on_request(ch, method, properties, body):
 	Body = body.decode("utf-8").split("!@!")
 	user = (Body[0],)
 
+	mycursor.execute(comand, user)
+	myresult = mycursor.fetchall()
+
+	print(myresult)
+
+	for linha in myresult:
+			if linha[1] == Body[1]:
+				resposta = "True!@!QS2BP7G39nzhdu4suPdy8cGkPVymvxzr"
+
 	try:
 		mycursor.execute(comand, user)
 		myresult = mycursor.fetchall()
